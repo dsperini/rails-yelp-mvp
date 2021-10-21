4c5a40ffc1e96d4372e08b9b6d8a303177ce2db7
Daniel Perini <dsperini@gmail.com>
2021-10-21 17:38:54 -0300

    validations implemented in model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,21 +1,11 @@
 class Restaurant < ApplicationRecord
-# A restaurant must have a name, an address and a category.
-validates :name, presence: true
-validates :address, presence: true
-validates :category, presence: true
+   # When a restaurant is destroyed, all of its reviews must be destroyed as well.
+  has_many :reviews, dependent: :destroy
+  # A restaurant must have a name, an address and a category.
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
 
-# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
-validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
-
-# When a restaurant is destroyed, all of its reviews must be destroyed as well.
-
-
-# A review must belong to a restaurant.
-
-
-# A review must have content and a rating.
-
-
-# A review’s rating must be a number between 0 and 5.
-
+  # A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ApplicationRecord
+  # A review must belong to a restaurant.
   belongs_to :restaurant
+
+  # A review must have content and a rating.
+  validates :content, presence: true
+  validates :rating, presence: true
+
+# A review’s rating must be a number between 0 and 5.
+  validates :rating, numericality: true
+  validates :rating, inclusion: 0..5
 end
