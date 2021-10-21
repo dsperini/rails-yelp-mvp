d0627ef990d8b225d8ef1d818460c1912e049460
Daniel Perini <dsperini@gmail.com>
2021-10-21 17:59:02 -0300

    restaurant controller implemented

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+  before_action :find, only: [:show]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+    private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
+  # before show, edit, update and destroy run this code!
+  def find
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
 end
